4a32feebe9a4b45e175a9fe06c4e4bbed786565e
Jeong Seung Hwan <tmdghks5466@naver.com>
2017-07-10 02:29:38 +0900

    db에 저장, db에서 값 가져와서 보여주기까지

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,13 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list) # 꺽쇠가 문자열의 시작, 달러가 문자열의 끝
+    
+    url(r'^insert/(?P<isbn>.+);(?P<title>.+);(?P<memo>.*)', 
+    views.InsertBook), # 왜 사이트는 MyAppView라고햇찌 ㅅㅂ
+
+    url(r'^$', views.post_list)
+   
+     # 꺽쇠가 문자열의 시작, 달러가 문자열의 끝
 ]
 # views.post_list를 호출하겠다 ()인 함수를 호출한게아니라 함수 자체를 넘겨줌
 
