f4122933fb420ac9457aacae971a0b73945265b6
Jeong Seung Hwan <tmdghks5466@naver.com>
2017-07-27 15:03:44 +0900

    dojo views.mysum에서 x/y/z인자 3개처리

--- dojo/urls.py
+++ dojo/urls.py
@@ -1,5 +1,11 @@
 from django.conf.urls import url # url 패턴 쓰기위해 임포트
-
+from . import views
 urlpatterns = [
-    
+    url(r'^sum/(?P<x>\d+)/$', views.mysum) , 
+    # 만약 여기서 $를 써주지않으면 그냥 무조건 주소중에 숫자 하나만! 들어오면 바로 이게 실행됨. 즉 숫자 두개 100/200 이렇게들어오는것도
+    # 다 첫번째 url패턴에서 처리하는것. 원래는 다음으로 넘어가야되는데 .
+    url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/$', views.mysum) ,
+    url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/(?P<z>\d+)/$', views.mysum) 
+    # views를 임포트해야 쓸수있음. 
+    # dojo 나오고, sum 나오고 숫자!! 나오면 view.mysum이 호출된다는 말. 
 ]
--- dojo/views.py
+++ dojo/views.py
@@ -1,3 +1,10 @@
+# dojo 밑의 view.py 파일.
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def mysum(request, x, y=0, z=0): # 디폴트 인자를 지정. 그니까 안넘어와도 디폴트값이 자동으로 되는거 .씹굿. 
+    # request: HTTPRequest 
+
+    return HttpResponse(int(x) + int(y) + int(z)) # httpResponse 임포트 해주고 써야함. 
+# HttpResponse(x+100)으로 하면 안되는 이유는 x는 무조건 문자열이기 때문. 
+# 그래서 x를 int 로 바꿔주고 하면 되네 . 
