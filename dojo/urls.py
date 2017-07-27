a600f11f01f1ba9deca46f2beb750c04890cb1e9
Jeong Seung Hwan <tmdghks5466@naver.com>
2017-07-27 15:18:39 +0900

    dojo앱에서 다수의 숫자 처리 ,슬래쉬처리

--- dojo/urls.py
+++ dojo/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import url # url 패턴 쓰기위해 임포트
 from . import views
 urlpatterns = [
+    
+    #존나 많이 / 슬래쉬 숫자 받아도 다 더할수있도록
+    url(r'^sum/(?P<numbers>[\d/]+)$',views.mysum2), # 정규표현식을 느슨하게.. / 도 표현되도록?
+
     url(r'^sum/(?P<x>\d+)/$', views.mysum) , 
     # 만약 여기서 $를 써주지않으면 그냥 무조건 주소중에 숫자 하나만! 들어오면 바로 이게 실행됨. 즉 숫자 두개 100/200 이렇게들어오는것도
     # 다 첫번째 url패턴에서 처리하는것. 원래는 다음으로 넘어가야되는데 .
     url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/$', views.mysum) ,
-    url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/(?P<z>\d+)/$', views.mysum) 
+    url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/(?P<z>\d+)/$', views.mysum), 
     # views를 임포트해야 쓸수있음. 
     # dojo 나오고, sum 나오고 숫자!! 나오면 view.mysum이 호출된다는 말. 
 ]
--- dojo/views.py
+++ dojo/views.py
@@ -2,6 +2,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
+def mysum2(request,numbers):
+    result = sum(map(lambda s : int(s or 0), numbers.split("/")))
+    # 스플릿하고, map으로 모두 int로 바꿔줌. 
+    # 그다음 다 더해주고 리턴. 
+    # 람다식을 추가해줬는데 이건, 슬래쉬가 // 두번들어갈경우 스플릿하면 빈문자열이 나오는데
+    # 이 빈문자열은 int로 바꾸어주지 못한다 그래서 오류발생
+    # 그래서 lambda식을 이용해서 or! 를 이용해서 거짓->이면 0으로 써라 라고 지정해준것. 
+    return HttpResponse(result)
 def mysum(request, x, y=0, z=0): # 디폴트 인자를 지정. 그니까 안넘어와도 디폴트값이 자동으로 되는거 .씹굿. 
     # request: HTTPRequest 
 
