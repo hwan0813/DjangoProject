5dfdb971ba8678d5d91028ea8016214aa41a889d
Jeong Seung Hwan <tmdghks5466@naver.com>
2017-08-01 17:19:40 +0900

    post모델에 status필드를 추가하고 이르 ㄹ처리하는 action어드민 구현'

--- dojo/admin.py
+++ dojo/admin.py
@@ -5,12 +5,27 @@ from .models import Post
 #그냥 단순 등록이아니라 이렇게 하면 admin 계정에서 웹으로 관리할때 내용이 상세하게 보임. 
 @admin.register(Post) #장식자 형태로 쓴것. ... 까먹음. 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title','content_size' ,'created_at', 'updated_at'
+    list_display = ['id', 'title','status','content_size' ,'created_at', 'updated_at'
     ]
+    actions = ['make_published','make_draft'] # 이렇게 액션 등록해야 만든 액션이 등록됨. 
 
     def content_size(self,post):
         return mark_safe('<strong>{}</strong>글자'.format(len(post.content)))
     content_size.short_description = '글자수' # 어드민관리하는쪽에 '글자수'로 보이게됨.
     #content_size.allow_tags = True # 이거쓰면 저기 <strong>태그가 적용됨. ->deprecated된기능임. 이제 마크세이프씀
     
-# admin.site.register(Post, PostAdmin)#어드민에 등록.
+    def make_published(self, request, queryset): #어드민에서 액션을 만드는 과정.
+    # 리퀘스트와 쿼리셋이 잇어야 하며 이 액션은 status를 published로 바꾸기위한 작업. 하나하나 변경하는 것이 아니라
+    # 한번에 체크리스트를 체크하고 한번에 바꾸기위한 작업. 
+        updated_count = queryset.update(status='p')
+        self.message_user(request, '{}건의 포스팅을 퍼블리쉬드로 변경'.format(updated_count))
+    make_published.short_description = '지정 포스팅을 published로 변경'
+
+
+    def make_draft(self, request, queryset): 
+        updated_count = queryset.update(status='d')
+        self.message_user(request, '{}건의 포스팅을 draft 변경'.format(updated_count))
+    make_draft.short_description = '지정 포스팅을 draft로 변경'
+
+
+# admin.site.register(Post, PostAdmin)# 어드민에 등록. 제일 기본적인 등록법. 
--- dojo/models.py
+++ dojo/models.py
@@ -8,6 +8,11 @@ def lnglat_validator(value):
     if not re.match(r'^([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)$', value):
         raise ValidationError('invalid LngLat Type')
 
+STATUS_CHOICES = (
+    ('d', 'Draft'),
+    ('p', 'Published'),
+    ('w', 'Withdrawn'),
+)
 
 class Post(models.Model):
     title = models.CharField(max_length=100,
@@ -20,7 +25,9 @@ class Post(models.Model):
     content = models.TextField(verbose_name="내용" , 
         help_text='여기에 내용을 입력하세요!') #verbose네임하면 필드입력이 이렇게 보여짐
         #help 텍스트하면 입력폼위에 회색글씨 작은걸로 도움말. 
-    #길이 제한이 없는 문자열 -> 텍스트필드, 데이터베이스는 엄연히 다르니까. 
+    #길이 제한이 없는 문자열 -> 텍스트필드, 데이터베이스는 엄연히 다르니까.
+    
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
     tags = models.CharField(max_length=100, blank=True)
     lnglat = models.CharField(max_length=50, help_text='경도/위도 포맷으로 입력',
          blank=True, validators=[lnglat_validator] ) # 블랭크 트루하면 값 안넣어도 ㅇㅋ.
