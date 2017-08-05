510fc56335d2fedba25d9983be93508f198459ed
Jeong Seung Hwan <tmdghks5466@naver.com>
2017-08-05 16:50:05 +0900

    모델폼 적용함. 모델을 가져와서 자동으로 폼을 형성하게 하는??/

--- blog/forms.py
+++ blog/forms.py
@@ -1,10 +1,20 @@
 from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        # 자동으로 모델가져와서 폼형식으로 채워줌. 여기 밑에 채우는 형식대로 입력창생김. 
+        fields = ['title', 'content']
+
+    #title = forms.CharField(validators=[min_length_3_validator])
+    #content = forms.CharField(widget=forms.Textarea)
 
 
-def min_length_3_validator(value):
-    if len(value)<3:
-        raise forms.ValidationError('3글자 이상 입력해주세요')
-
-class PostForm(forms.Form):
-    title = forms.CharField(validators=[min_length_3_validator])
-    content = forms.CharField(widget=forms.Textarea)
+    '''
+    def save(self, commit = True):
+        self.instance = Post(**self.cleaned_data)
+        if commit:
+            self.instance.save()
+        return self.instance
+    ''' 
--- blog/views.py
+++ blog/views.py
@@ -41,9 +41,13 @@ def post_new(request):
            # post = Post.objects.create(title=form.cleaned_data['title'],
            #                    content = form.cleaned_data['content']) 
            # 방법4
-           post = Post.objects.create(**form.cleaned_data)
+           #post = Post.objects.create(**form.cleaned_data)
+           #return redirect('/blog/')
+           # 이장석이 전호하는건 create를 forms.py안으로 옮기는것.
+           post = form.save(commit=False) # 밑에 post.save()에서 저장일어나니까 커밋 false로해서 저장안되게??
+           post.ip = request.META['REMOTE_ADDR']
+           post.save()
            return redirect('/blog/')
-    
     else:
         form = PostForm()
     return render(request, 'blog/post_form.html', { 
