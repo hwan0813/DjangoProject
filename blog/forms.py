43e9aba86ae7ba90e640ecf19cc5da9f356af054
Jeong Seung Hwan <tmdghks5466@naver.com>
2017-08-06 17:33:41 +0900

    blog Model에 user_agent필드추가

--- blog/forms.py
+++ blog/forms.py
@@ -5,7 +5,10 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         # 자동으로 모델가져와서 폼형식으로 채워줌. 여기 밑에 채우는 형식대로 입력창생김. 
-        fields = ['title', 'content']
+        fields = ['title', 'content','user_agent']
+        widgets ={
+            'user_agent' : forms.HiddenInput,
+        }
 
     #title = forms.CharField(validators=[min_length_3_validator])
     #content = forms.CharField(widget=forms.Textarea)
