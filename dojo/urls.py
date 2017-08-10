ff63d26aae0bb5738c40c5d5adabb0aaba41ffd4
Jeong Seung Hwan <tmdghks5466@naver.com>
2017-08-10 15:50:06 +0900

    CBV: DeleteView 구현

--- dojo/urls.py
+++ dojo/urls.py
@@ -33,5 +33,6 @@ urlpatterns = [
 
     url(r'^cbv/new/$', views_cbv.post_new),
     url(r'^cbv/(?P<pk>\d+)/edit/$', views_cbv.post_edit),
+    url(r'^cbv/(?P<pk>\d+)/delete/$', views_cbv.post_delete),
 
 ]
--- dojo/views_cbv.py
+++ dojo/views_cbv.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse, JsonResponse
-from django.views.generic import View,TemplateView , ListView, DetailView, UpdateView, CreateView
+from django.views.generic import View,TemplateView , ListView, DetailView, UpdateView, CreateView, DeleteView
 from .models import Post
 
+post_delete = DeleteView.as_view(model=Post, success_url = '/dojo/')
 post_new = CreateView.as_view(model=Post)
 post_edit = UpdateView.as_view(model=Post, fields='__all__')
 
