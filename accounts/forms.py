895b1fe1fe04d30e6d7790c00d63b01c117aa403
Jeong Seung Hwan <tmdghks5466@naver.com>
2017-08-11 17:51:30 +0900

    퀴즈를 맞추야 로그인 - 로그인 커스터마이징'

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Profile
 
 # 장고에서 기본제공하는 UserCreationForm상속받아서
@@ -13,6 +13,7 @@ class SignupForm(UserCreationForm):
 
     #phone_number나 address는 기존의 User
     #creation폼에 없으므로 따로 저장구현해줘야함
+    # pylint: disable=E1101
     def save(self):
         user = super().save()
         profile = Profile.objects.create(
@@ -20,3 +21,12 @@ class SignupForm(UserCreationForm):
             phone_number = self.cleaned_data['phone_number'],
             address = self.cleaned_data['address'])
         return user
+
+class LoginForm(AuthenticationForm):
+    answer = forms.IntegerField(label='3+3=?')
+
+    def clean_answer(self):
+        answer = self.cleaned_data.get('answer',None)
+        if answer != 6:
+            raise forms.ValidationError('mismatched!')
+        return answer
