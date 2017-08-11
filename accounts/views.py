e3e2bd07418cdccc5448a52a552c793ca8fa1a9f
Jeong Seung Hwan <tmdghks5466@naver.com>
2017-08-11 16:46:52 +0900

    프로필 뷰에 login_required 장식자 추가

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from django.conf import settings
 
 def signup(request):
@@ -15,6 +16,8 @@ def signup(request):
         'form' : form,
     })
 
-
+#이 장식자는 아직 로그인안했는데 프로필을 보려고하면 굳이 화면에 띄워줄필요 x
+#그럴때 로그인을 강제하는 코드. 프로필 누르면 로그인화면으로 이동. 로그인성공시 프로필보여줌. 
+@login_required
 def profile(request):
     return render(request, 'accounts/profile.html')
